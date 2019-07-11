3852f308400601392584f637c452563f69a0614f
suchanyaaaaaaaa <51774075+suchanyaaaaaaaa@users.noreply.github.com>
2019-07-11 13:28:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,13 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    sendText(user,userText)
+    #sendText(user,userText)
+    if (usertext == 'ไปเถอะ') :
+        sendText(user,'ไปไหนอ่ะ')
+    elif (usertext == 'ไปนะ') :
+        sendText(useprint('ชวนจริ๊ง')
+    else :
+        sendText(user,'ว่าไงนะ')
     return '',200
 
 def sendText(user, text):
